71ca80763598fd10b4c311269d3900187828fc9d
Bruno de Queiroz <bruno.dqueirozmt@gmail.com>
2025-09-03 11:16:21 -0300

    vamo ver né

--- QuemVemAI/settings.py
+++ QuemVemAI/settings.py
@@ -95,7 +95,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://emeninooumenina-homologacao.up.railway.app"
 ]
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", os.getenv("DJANGO_SECRET_KEY"))
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
